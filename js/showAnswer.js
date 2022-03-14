5f89ede7c5d507f9f5d06239f6080d5339d816f4
Alexander Fischenich <alexf@airvonalexander.speedport.ip>
2022-03-14 17:46:05 +0100

    add js for index and bookmarks
    add js to toggle  the answers

---
+++ js/showAnswer.js
@@ -0,0 +1,31 @@
+document
+  .querySelector("button[data-button-value='answer__button--toggle1']")
+  .addEventListener("click", function () {
+    document
+      .querySelector(".card__answer--show1")
+      .classList.toggle("card__answer--showAnswer");
+  });
+
+document
+  .querySelector("button[data-button-value='answer__button--toggle2']")
+  .addEventListener("click", function () {
+    document
+      .querySelector(".card__answer--show2")
+      .classList.toggle("card__answer--showAnswer");
+  });
+
+document
+  .querySelector("button[data-button-value='answer__button--toggle3']")
+  .addEventListener("click", function () {
+    document
+      .querySelector(".card__answer--show3")
+      .classList.toggle("card__answer--showAnswer");
+  });
+
+document
+  .querySelector("button[data-button-value='answer__button--toggle4']")
+  .addEventListener("click", function () {
+    document
+      .querySelector(".card__answer--show4")
+      .classList.toggle("card__answer--showAnswer");
+  });
