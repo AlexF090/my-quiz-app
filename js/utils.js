08c5b87763513cef42f86561db516a208419aee0
Alexander Fischenich <alexf@airvonalexander.speedport.ip>
2022-03-18 18:06:56 +0100

    add first counter to textarea

---
+++ js/utils.js
@@ -0,0 +1,1 @@
+// Counter function for textarea
--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-import { xyz } from './js/utils';
+//import { xyz } from './js/utils';
+
+//export
+
+const question2 = document.getElementById('question');
+question2.addEventListener('input', () => {
+  const count = question2.value.length;
+  document.getElementById('charCount').textContent = ` ${count}`;
+});
+
+/*
+question2.addEventListener("input", charCounter(currywurst, charCounter))
+
+function charCounter (currywurst, charCount) {
+const textarea = document.getElementById(currywurst);
+const count = textarea.value.length;
+document.getElementById(charCount).textContent = ` ${count}`;
+};
+*/
