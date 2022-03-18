67c2f51fd91531393ca6e85d8d0e6bda5149a924
Alexander Fischenich <alexf@airvonalexander.speedport.ip>
2022-03-18 18:16:16 +0100

    update counter

--- main.js
+++ main.js
@@ -2,10 +2,25 @@
 
 //export
 
-const question2 = document.getElementById('question');
-question2.addEventListener('input', () => {
-  const count = question2.value.length;
-  document.getElementById('charCount').textContent = ` ${count}`;
+//Question Counter
+const question1 = document.getElementById('question');
+question1.addEventListener('input', () => {
+  const count = question1.value.length;
+  document.getElementById('charCount1').textContent = ` ${count} sign(s)`;
+});
+
+//Answer Counter
+const answer = document.getElementById('answer');
+answer.addEventListener('input', () => {
+  const count = answer.value.length;
+  document.getElementById('charCount2').textContent = ` ${count} sign(s)`;
+});
+
+//Tags Counter
+const tags = document.getElementById('tags');
+tags.addEventListener('input', () => {
+  const count = tags.value.length;
+  document.getElementById('charCount3').textContent = ` ${count} sign(s)`;
 });
 
 /*
