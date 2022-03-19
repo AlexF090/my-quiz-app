db9694169a61d99e128a7150733d3eafd037fd15
Alexander Fischenich <alexf@airvonalexander.speedport.ip>
2022-03-19 22:20:48 +0100

    add hide

--- main.js
+++ main.js
@@ -32,3 +32,94 @@ const count = textarea.value.length;
 document.getElementById(charCount).textContent = ` ${count}`;
 };
 */
+
+// Display the right Page
+
+// const buttons = document.querySelectorAll('.navList li');
+// const article = document.querySelectorAll('.article');
+
+// buttons.forEach((item, index) => {
+// item.addEventListener('click', () => {
+// article.forEach(item => {
+// item.style.display = "none';
+// });
+// article[index].style.display = 'block';
+// });
+// });
+
+// const buttons = document.querySelectorAll('.navList li');
+// const pages = document.querySelectorAll('.article');
+// const hide = document.getElementsByClassName('hide');
+
+// // console.log(buttons);
+
+// buttons.forEach(buttons => {
+//   console.log(buttons);
+//   hide.classList.toggle('show');
+// });
+
+// document.querySelectorAll(buttons).forEach(el => {
+//   el.addEventListener('click', e => {
+//     if(buttons.classList()) el.querySelector('article').classList.toggle('.show');
+//   });
+// })
+
+//  document.querySelectorAll(navList).forEach(el => {}
+
+//    );
+
+// const nav = document.querySelectorAll('.navList li');
+// console.log(nav);
+
+// nav.forEach(listItem => {
+
+//   listItem.addEventListener('click', () => {
+//     const articles = document.querySelectorAll('article');
+//     articles.forEach(item, index => {
+//       item.classList.add('hide');
+//     })
+//   });
+// });
+
+const buttons = document.querySelectorAll('.navList li');
+const article = document.querySelectorAll('article');
+
+buttons.forEach((item, index) => {
+  item.addEventListener('click', () => {
+    article.forEach(item => {
+      item.classList.add('hide');
+    });
+    // console.log(article[index]);
+    article[index].classList.remove('hide');
+  });
+});
+
+/* OLD!!!!!!!!!!!!!!!
+const nav = document.querySelectorAll('.navList li');
+console.log(nav);
+
+nav.forEach(listItem => {
+  listItem.addEventListener('click', () => {
+    const articles = document.querySelectorAll('article');
+    articles.forEach(item => {
+      if (item.target.getAttribute('quiz-app') === true) {
+        const articleQuizApp = document.querySelector(
+          'article[data-title="quiz-app"]'
+        );
+        articleQuizApp.classList.add('show');
+      }
+      // if (data-nav === 'bookmarks') {
+      //   const articleQuizApp = document.querySelector(
+      //     'article[data-title="bookmarks"]'
+      //   );
+      //   articleQuizApp.classList.add('show');
+      // }
+      // if (data-nav === 'create') {
+      //   const articleQuizApp = document.querySelector('article[data-title="create"]');
+      //   articleQuizApp.classList.add('show');
+      // }
+      // if (data-nav === 'profile') {
+      //   const articleQuizApp = document.querySelector('article[data-title="profile"]');
+      //   articleQuizApp.classList.add('show');
+      // }
+      */
