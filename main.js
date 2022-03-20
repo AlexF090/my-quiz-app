193eac04518f3339833f5ae2837a3bd2725911e0
Alexander Fischenich <alexf@airvonalexander.speedport.ip>
2022-03-20 11:42:45 +0100

    add icon change in js
    the new feature is if you click on a icon in nav the color change to them

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 //import { xyz } from './js/utils';
 
+// const { config } = require("@storybook/addon-actions");
+
 //export
 
 //Question Counter
@@ -23,103 +25,20 @@ tags.addEventListener('input', () => {
   document.getElementById('charCount3').textContent = ` ${count} sign(s)`;
 });
 
-/*
-question2.addEventListener("input", charCounter(currywurst, charCounter))
-
-function charCounter (currywurst, charCount) {
-const textarea = document.getElementById(currywurst);
-const count = textarea.value.length;
-document.getElementById(charCount).textContent = ` ${count}`;
-};
-*/
-
-// Display the right Page
-
-// const buttons = document.querySelectorAll('.navList li');
-// const article = document.querySelectorAll('.article');
-
-// buttons.forEach((item, index) => {
-// item.addEventListener('click', () => {
-// article.forEach(item => {
-// item.style.display = "none';
-// });
-// article[index].style.display = 'block';
-// });
-// });
-
-// const buttons = document.querySelectorAll('.navList li');
-// const pages = document.querySelectorAll('.article');
-// const hide = document.getElementsByClassName('hide');
-
-// // console.log(buttons);
-
-// buttons.forEach(buttons => {
-//   console.log(buttons);
-//   hide.classList.toggle('show');
-// });
-
-// document.querySelectorAll(buttons).forEach(el => {
-//   el.addEventListener('click', e => {
-//     if(buttons.classList()) el.querySelector('article').classList.toggle('.show');
-//   });
-// })
-
-//  document.querySelectorAll(navList).forEach(el => {}
-
-//    );
-
-// const nav = document.querySelectorAll('.navList li');
-// console.log(nav);
-
-// nav.forEach(listItem => {
-
-//   listItem.addEventListener('click', () => {
-//     const articles = document.querySelectorAll('article');
-//     articles.forEach(item, index => {
-//       item.classList.add('hide');
-//     })
-//   });
-// });
-
-const buttons = document.querySelectorAll('.navList li');
-const article = document.querySelectorAll('article');
+const buttons = document.querySelectorAll('.nav_icon'),
+  article = document.querySelectorAll('article');
 
 buttons.forEach((item, index) => {
   item.addEventListener('click', () => {
     article.forEach(item => {
       item.classList.add('hide');
+      buttons[index].classList.remove('current');
     });
-    // console.log(article[index]);
+    buttons.forEach(item => {
+      item.classList.remove('current');
+    });
+    console.log(article[index]);
     article[index].classList.remove('hide');
+    buttons[index].classList.add('current');
   });
 });
-
-/* OLD!!!!!!!!!!!!!!!
-const nav = document.querySelectorAll('.navList li');
-console.log(nav);
-
-nav.forEach(listItem => {
-  listItem.addEventListener('click', () => {
-    const articles = document.querySelectorAll('article');
-    articles.forEach(item => {
-      if (item.target.getAttribute('quiz-app') === true) {
-        const articleQuizApp = document.querySelector(
-          'article[data-title="quiz-app"]'
-        );
-        articleQuizApp.classList.add('show');
-      }
-      // if (data-nav === 'bookmarks') {
-      //   const articleQuizApp = document.querySelector(
-      //     'article[data-title="bookmarks"]'
-      //   );
-      //   articleQuizApp.classList.add('show');
-      // }
-      // if (data-nav === 'create') {
-      //   const articleQuizApp = document.querySelector('article[data-title="create"]');
-      //   articleQuizApp.classList.add('show');
-      // }
-      // if (data-nav === 'profile') {
-      //   const articleQuizApp = document.querySelector('article[data-title="profile"]');
-      //   articleQuizApp.classList.add('show');
-      // }
-      */
