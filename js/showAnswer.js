c521705cafc8362070721cf3fdb079e61f1200fb
Alexander Fischenich <alexf@airvonalexander.speedport.ip>
2022-03-21 12:40:55 +0100

    update js  open answer with button

--- js/showAnswer.js
+++ js/showAnswer.js
@@ -1,31 +1,9 @@
-document
-  .querySelector("button[data-button-value='answer__button--toggle1']")
-  .addEventListener('click', function () {
-    document
-      .querySelector('.card__answer--show1') /* Hier paragraph*/
-      .classList.toggle('card__answer--showAnswer'); /* Hier class*/
-  });
+const cards = document.querySelectorAll('.card');
 
-document
-  .querySelector("button[data-button-value='answer__button--toggle2']")
-  .addEventListener('click', function () {
-    document
-      .querySelector('.card__answer--show2')
-      .classList.toggle('card__answer--showAnswer');
+cards.forEach(card => {
+  const button = card.querySelector('.answerButton');
+  const answer = card.querySelector('.card__answer');
+  button.addEventListener('click', () => {
+    answer.classList.toggle('card__answer--showAnswer');
   });
-
-document
-  .querySelector("button[data-button-value='answer__button--toggle3']")
-  .addEventListener('click', function () {
-    document
-      .querySelector('.card__answer--show3')
-      .classList.toggle('card__answer--showAnswer');
-  });
-
-document
-  .querySelector("button[data-button-value='answer__button--toggle4']")
-  .addEventListener('click', function () {
-    document
-      .querySelector('.card__answer--show4')
-      .classList.toggle('card__answer--showAnswer');
-  });
+});
